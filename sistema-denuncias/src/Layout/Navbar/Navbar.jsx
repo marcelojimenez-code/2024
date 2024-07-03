3a356f1af3b29cdca184098c8ad32f9eae807c4b
Marcelo Jimenez <marcelojimenezcubillos@gmail.com>
2024-07-03 17:48:07 -0400

    actualizado

--- sistema-denuncias/src/Layout/Navbar/Navbar.jsx
+++ sistema-denuncias/src/Layout/Navbar/Navbar.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="#ef6c00 orange darken-3 white" role="navigation">
-    <div className="nav-wrapper">
-      <ul className="left hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
-        <li><a href="#">Navbar Link</a></li>
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
+     <nav className="orange" role="navigation">
+        <div className="nav-wrapper">
+          <ul className="left hide-on-med-and-down">
+            <li><a href="#">Registro Denuncias</a></li>
+            <li><a href="#">Gestor Denuncias</a></li>
+            <li><a href="#">Registro CJS</a></li>
+          </ul>
 
-      <ul id="nav-mobile" className="sidenav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-    </div>
-  </nav>
+          <ul id="nav-mobile" className="sidenav">
+            <li><a href="#">Navbar Link</a></li>
+          </ul>
+          <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        </div>
+     </nav>
   )
 }
 
